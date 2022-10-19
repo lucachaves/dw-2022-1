9b2350986996f7bbd7d571f0ec393a1ba814b756
Luiz <lucachaves@gmail.com>
2022-10-19 10:52:54 -0300

    system call

---
+++ project/monitor-app/src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import morgan from 'morgan';
+
+import routes from './routes.js';
+
+const port = 3000;
+
+const app = express();
+
+app.use(morgan('tiny'));
+
+app.use(routes);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
