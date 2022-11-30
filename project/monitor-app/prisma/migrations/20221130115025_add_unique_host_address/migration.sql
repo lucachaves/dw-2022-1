f73fdb0705faed0b071a4e53daaeed02d2be924d
Luiz <lucachaves@gmail.com>
2022-11-30 10:43:01 -0300

    initial config

--- project/monitor-app/prisma/migrations/20221130115025_add_unique_host_address/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[address]` on the table `Host` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Host_address_key" ON "Host"("address");
